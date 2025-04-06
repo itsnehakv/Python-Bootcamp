620caa8220d588251131b5d0ba7b76abc3ceed01
Neha KV <itsnehakv@gmail.com>
2025-04-06 23:36:17 +0530

    Update additional-info.py

--- nato-iterrows-26-/additional-info.py
+++ nato-iterrows-26-/additional-info.py
@@ -5,18 +5,18 @@ student_dict = {
 
 #Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+    print(key)
 
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+    print(row)
+    print(row.score)
+    if row.student=="Angela":
+        print(row.score)
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# Keyword Method with iterrows(). Creating dictionary by iterating through dataframe (df).
+{new_key:new_value for (index, row) in df.iterrows()}
 
